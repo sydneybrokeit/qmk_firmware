b2d5451cf7ea7302b40765784bb86ff746c0d4a1
JX <572673807@qq.com>
2022-08-23 01:42:11 +0800

    更新6095和bl75的via文件

--- keyboards/yandrstudio/wired/bl75/config.h
+++ keyboards/yandrstudio/wired/bl75/config.h
@@ -55,7 +55,7 @@
 #ifdef RGBLIGHT_ENABLE
 
 #    define RGB_DI_PIN A7
-#    define RGBLED_NUM 84
+#    define RGBLED_NUM 116
 #    define DRIVER_LED_TOTAL RGBLED_NUM
 #    define RGBLIGHT_SLEEP
 #    define RGBLIGHT_VAL_STEP 5
@@ -81,10 +81,10 @@
 #ifdef RGB_MATRIX_ENABLE
 
 #    define RGB_DI_PIN A7
-#    define RGBLED_NUM 84
+#    define RGBLED_NUM 116
 #    define DRIVER_LED_TOTAL RGBLED_NUM
 
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
 #    define RGBLIGHT_VAL_STEP 5
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGB_MATRIX_CENTER { 112, 32 }
