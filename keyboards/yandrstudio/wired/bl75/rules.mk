e677c6feafe93f890785f7f9b98354df225f64b7
JX <572673807@qq.com>
2022-10-10 20:31:33 +0800

    更新bl75

--- keyboards/yandrstudio/wired/bl75/rules.mk
+++ keyboards/yandrstudio/wired/bl75/rules.mk
@@ -16,3 +16,5 @@ RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 # RGBLIGHT_ENABLE = yes
 # RGBLIGHT_DRIVER = WS2812
+CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
+QUANTUM_SRC += matrix.c
