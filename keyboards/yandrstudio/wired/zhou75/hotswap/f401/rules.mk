92e8b773977dc673869f940df469617fe2273f24
JX <572673807@qq.com>
2022-10-13 18:04:30 +0800

    up zhou75

--- keyboards/yandrstudio/wired/zhou75/hotswap/f401/rules.mk
+++ keyboards/yandrstudio/wired/zhou75/hotswap/f401/rules.mk
@@ -2,7 +2,8 @@
 MCU = STM32F401
 
 # Bootloader selection
-BOOTLOADER = stm32-dfu
+# BOOTLOADER = stm32-dfu
+BOOTLOADER = tinyuf2
 
 # # project specific file
 CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
@@ -15,12 +16,3 @@ RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = pwm
 
 ENCODER_ENABLE = yes
-
-# EEPROM_DRIVER = spi
-EEPROM_DRIVER = custom
-SRC += eep/eeprom_stm32.c
-SRC += eep/flash_stm32.c
-OPT_DEFS += -DEEPROM_EMU_STM32F401xC
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
