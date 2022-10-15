16d9bb068a8fbb5993f79ff0512c05b5aecb0c2a
JX <572673807@qq.com>
2022-10-16 01:57:37 +0800

    up buff67v3

--- keyboards/yandrstudio/wired/buff67v3/config.h
+++ keyboards/yandrstudio/wired/buff67v3/config.h
@@ -35,19 +35,8 @@
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define TAP_CODE_DELAY 15
-
-// enable the nkro when using the VIA.
-#define FORCE_NKRO
-
-#define HOLD_ON_OTHER_KEY_PRESS
-#define TAP_HOLD_CAPS_DELAY 250
-
-// fix VIA RGB_light
-#define VIA_HAS_BROKEN_KEYCODES
-
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 20
+#define DEBOUNCE 8
 
 #ifdef RGBLIGHT_ENABLE
 
