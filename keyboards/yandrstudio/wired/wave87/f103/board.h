7f5f3fd5ad2d6c82621e2089cb5125ad40fb1eb6
JX <572673807@qq.com>
2022-10-20 23:48:01 +0800

    up wave87 scan way

--- keyboards/yandrstudio/wired/wave87/f103/board.h
+++ keyboards/yandrstudio/wired/wave87/f103/board.h
@@ -17,6 +17,14 @@
 
 #include_next "board.h"
 
+#undef VAL_GPIOACRL
+#define VAL_GPIOACRL 0x88888888
+
+#undef VAL_GPIOBCRH
+#define VAL_GPIOBCRH 0x88888888
+
+#undef VAL_GPIOCCRH
+#define VAL_GPIOCCRH 0x88888888
 
 #undef STM32_HSECLK
 #define STM32_HSECLK 16000000
